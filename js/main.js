e4cd2aafce819920b79866149a33114e951a70af
sinadaryaei <sinatangssiran@gmail.com>
2023-05-31 16:31:25 +0000

    Queried form, listened to 'submit' prevent page from refreshing content | (user-can-create-an-entry).

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 const $imgInput = document.getElementById('url');
 const $imgPreview = document.querySelector('img');
+const $form = document.querySelector('[data-view="entry-form"]');
 
 $imgInput.addEventListener('input', event => {
   $imgPreview.setAttribute('src', $imgInput.value);
+});
+
+$form.addEventListener('submit', event => {
+  event.preventDefault();
+
 });
