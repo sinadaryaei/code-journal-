32a57443240c10d88a81911ecba371884264b017
sinadaryaei <sinatangssiran@gmail.com>
2023-05-31 17:09:59 +0000

    Queried inputs assigned those inputs value to newObject incremented added newObject to the begging of the data entries array reset the  and  | (user-can-create-an-entry).

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 const $imgInput = document.getElementById('url');
 const $imgPreview = document.querySelector('img');
-const $form = document.querySelector('[data-view="entry-form"]');
+const $form = document.querySelector('form');
+const $title = document.getElementById('title');
+const $url = document.getElementById('url');
+const $notes = document.getElementById('notes');
 
 $imgInput.addEventListener('input', event => {
   $imgPreview.setAttribute('src', $imgInput.value);
@@ -9,4 +12,16 @@ $imgInput.addEventListener('input', event => {
 $form.addEventListener('submit', event => {
   event.preventDefault();
 
+  const newObject = {
+    title: $title.value,
+    url: $url.value,
+    notes: $notes.value,
+    entryId: data.nextEntryId
+  };
+
+  data.nextEntryId++;
+
+  data.entries.unshift(newObject);
+  $imgPreview.src = 'images/placeholder-image-square.jpg';
+  $form.reset();
 });
